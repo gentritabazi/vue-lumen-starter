7b55b15e39f89351f4e88f982f63245c4b02a1ba
Gentrit Abazi <gentritabazi01@gmail.com>
2020-04-10 17:51:49 +0200

    Lumen Updated to 7.0

--- back/app/Exceptions/Handler.php
+++ back/app/Exceptions/Handler.php
@@ -2,12 +2,12 @@
 
 namespace App\Exceptions;
 
-use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -28,10 +28,12 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return void
+     *
+     * @throws \Exception
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
@@ -40,20 +42,13 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Throwable
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
-
-         // exception for TokenBlacklistedException
-    if ($exception instanceof TokenBlacklistedException) {
-        return response()->json([
-            "error" => "token blacklisted"
-        ], 401);
-    }
-
-    
         return parent::render($request, $exception);
     }
 }
