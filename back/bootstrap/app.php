e0d2659c21426dd867f5a5533c5adbe053f64e1c
Gentrit Abazi <gentritabazi01@gmail.com>
2020-04-26 22:17:40 +0200

    AppServiceProvider and EventServiceProvider un-commented.

--- back/bootstrap/app.php
+++ back/bootstrap/app.php
@@ -76,9 +76,9 @@ $app->routeMiddleware([
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\EventServiceProvider::class);
 
 // Custom
 $app->register(\Tymon\JWTAuth\Providers\LumenServiceProvider::class);
