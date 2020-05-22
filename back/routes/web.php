1112f282bf3ca7ddd3d6bfad18b01333c6e1c35e
Gentrit Abazi <gentritabazi01@gmail.com>
2020-05-22 02:22:46 +0200

    Added hello-world test api for authenticated users.

--- back/routes/web.php
+++ back/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 // API
-$router->group(['prefix' => 'api'], function() use ($router) {
+$router->group(['prefix' => 'api'], function () use ($router) {
     // Auth
     $router->post('login', 'Auth\AuthController@login');
     $router->post('register', 'Auth\AuthController@register');
+
+    // Hello World
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        $router->get('hello-world', function () {
+            return 'hello world';
+        });
+    });
 });
