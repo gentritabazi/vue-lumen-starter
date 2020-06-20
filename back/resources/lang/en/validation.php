65faa477558992857a0d1abb974a03d238724343
Gentrit Abazi <gentritabazi01@gmail.com>
2020-06-20 18:54:34 +0200

    Added gender to attributes.

--- back/resources/lang/en/validation.php
+++ back/resources/lang/en/validation.php
@@ -149,7 +149,8 @@ return [
         'first_name' => 'First name',
         'last_name' => 'Last name',
         'email' => 'Email',
-        'password' => 'Password'
+        'password' => 'Password',
+        'gender' => 'Gender',
     ],
 
 ];
